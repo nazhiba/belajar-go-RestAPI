856b7daa6efb45ef263f0d9960873399c5536879
Nadzib <nadzib22@gmail.com>
2025-01-05 11:46:26 +0700

    refactor DBProductRepository to use mutex for thread-safe operations and initialize products in constructor

--- infrastructure/repository/db_repository.go
+++ infrastructure/repository/db_repository.go
@@ -1,16 +1,33 @@
 package repository
+import (
+	"go-test/domain"
+	"sync"
+)
 
-import "go-test/domain"
+type DBProductRepository struct {
+	mu sync.Mutex
+	products []domain.Product
+}
 
-type DBProductRepository struct {}
+func NewDBProductRepository() *DBProductRepository {
+	return &DBProductRepository{
+		products: []domain.Product{
+			{ID: 1, Name: "Laptop", Price: 1000},
+			{ID: 2, Name: "Mouse", Price: 25},
+		},
+	}
+}
 
 func (r *DBProductRepository) FindAll() ([]domain.Product, error) {
-	return []domain.Product{
-		{ID: 1, Name: "Laptop", Price: 1000},
-		{ID: 2, Name: "Mouse", Price: 25},
-			}, nil
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.products, nil
 }
 
 func (r *DBProductRepository) Save(product domain.Product) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.products = append(r.products, product)
 	return nil
 }
